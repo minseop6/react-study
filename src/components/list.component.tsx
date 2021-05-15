3a881785e2f404f2191bd2478aeca9d683442b8b
Teddy-Seop <teddy.kim@onda.me>
2021-05-15 11:45:18 +0900

    Add icon

--- src/components/list.component.tsx
+++ src/components/list.component.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CSS from 'csstype';
+import Icon from '@material-ui/core/Icon';
+import { Close, CheckBox, CheckBoxOutlineBlank } from '@material-ui/icons';
 
 export const List: React.FC = () => {
   const listStyle: CSS.Properties = {
@@ -8,13 +10,29 @@ export const List: React.FC = () => {
     width: '400px',
   };
 
+  const itemStyle: CSS.Properties = {
+    margin: '5px 0 0 0',
+    padding: '10px 0',
+    border: '1px solid black',
+  };
+
   const rawData = localStorage.getItem('todoList');
   const todoList: string[] = rawData ? JSON.parse(rawData) : [];
-  const items = todoList.map((item) => <li key={item}>{item}</li>);
+  const items = todoList.map((item) => (
+    <div key={item} style={itemStyle}>
+      <button type="button">
+        <Icon className="fa fa-plus-circle" />
+        {/* <CheckBox /> */}
+      </button>
+      <CheckBoxOutlineBlank />
+      <span>{item}</span>
+      <Close />
+    </div>
+  ));
 
   return (
     <div className="List" style={listStyle}>
-      <ul>{items}</ul>
+      {items}
     </div>
   );
 };
