d473619e7766e024d25fcf74991b1dd3edd1a2b8
Teddy-Seop <teddy.kim@onda.me>
2021-05-19 09:46:31 +0900

    Improvement todo list

--- src/components/list/list.component.tsx
+++ src/components/list/list.component.tsx
@@ -3,10 +3,13 @@ import './list.component.scss';
 import Icon from '@material-ui/core/Icon';
 import { ITodo } from '../input/input.component';
 
-export const List: React.FC = () => {
-  const rawData = localStorage.getItem('todoList');
-  const todoList: ITodo[] = rawData ? JSON.parse(rawData) : [];
-  const items = todoList.map((item) => (
+interface IListProps {
+  list: ITodo[];
+}
+
+export const List: React.FC<IListProps> = (props: IListProps) => {
+  const { list } = props;
+  const items = list.map((item) => (
     <div key={item.id} className={`item ${item.checked && 'complete'}`}>
       <input type="checkbox" className="checkbox" />
       <span>{item.text}</span>
--- src/components/todo-list/todo-list.component.tsx
+++ src/components/todo-list/todo-list.component.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { Input } from '../input/input.component';
 import { List } from '../list/list.component';
+import { RootState } from '../../reducers';
 
 export const TodoList: React.FC = () => {
+  const { todoList } = useSelector((state: RootState) => ({
+    todoList: state.todoReducer.todoList,
+  }));
   return (
     <div className="todoList">
       <Input />
-      <List />
+      <List list={todoList} />
     </div>
   );
 };
